7a73c605edcc381f6e9600234275b8af219ad8a2
balqis0-0 <bokubat@gmail.com>
2024-09-05 15:13:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function updateTime() {
   const timeElement = document.getElementById('time');
   const now = new Date();
-  const hours = string(now.getHours()).padStart(2, '0');
-  const minutes = string(now.getMinutes()).padStart(2, '0');
-  const seconds = string(now.getSeconds()).padStart(2, '0');
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
   timeElement.textContent = `${hours}:${minutes}:${seconds}`;
 }
 
